a5dc5615fbef505675014063b486c84f180a0f6c
JohanTT <thejohan39@gmail.com>
2022-11-24 09:42:17 +0700

    Add project files.

---
+++ GITTestNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GITTestNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
